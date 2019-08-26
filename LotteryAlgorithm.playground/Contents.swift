26d47cf4935fe73aed73448c7448b4eaa56c7630
Nathan <453816118@qq.com>
2019-08-26 16:46:02 +0800

    添加5分时时彩注数算法
    添加5分时时彩注数部分算法

--- LotteryAlgorithm.playground/Contents.swift
+++ LotteryAlgorithm.playground/Contents.swift
@@ -112,13 +112,13 @@ binomialCoefficient(8, choose: 5)
 //五星组选30
 //五星组选20
 //五星组选10
-// 组选不会
+
 
 //组选：四星组选24
 //组选：四星组选12
 //组选：四星组选6
 //组选：四星组选4
-// 组选不会
+
 
 // 不定位：一码不定位
 binomialCoefficient(6, choose: 1)
@@ -130,7 +130,8 @@ binomialCoefficient(6, choose: 2)
 binomialCoefficient(6, choose: 3)
 
 // 前三直选和值
-// 不会
+
+
 
 // 组选：组三
 let z3 = permutations(10, 2)
@@ -161,6 +162,189 @@ binomialCoefficient(6, choose: 1)
 binomialCoefficient(6, choose: 1)
 
 
+/********************5分时时彩**********************/
+//定位胆
+//注数 = 一个号码球为一注
+// max number is 50 bet
+let 万位 = permutations(n, 1) //n<10
+let 千位 = permutations(n, 1) //n<10
+let 百位 = permutations(n, 1) //n<10
+let 十位 = permutations(n, 1) //n<10
+let 个位 = permutations(n, 1) //n<10
+let 总注数 = 万位 + 千位 + 百位 + 十位 + 个位
+
+//前二
+//前二直选
+let 万位 = permutations(n, 1) //n<10
+let 千位 = permutations(n, 1) //n<10
+let 总注数 = 万位 * 千位
+
+// 直选和值 特殊处理
+
+// 前二组选 
+let result = binomialCoefficient(n, choose: 2) // prints 10 ,the n max less number 10
+
+// 前二直选单式
+// 将号码球按两两作为一注分割，然后遍历计算注数 例如：12 34，按空格分开。
+// n = [12,34]
+func 前二直选单式(_ n:[Int]) -> Int {
+        var answers:Int = 0
+        for i in n {
+            // i1 = 12  
+            // i2 = 34
+            if i.count != 2 return
+            let a = i.first.count // a1 printf 1 ; a2 printf 1 
+            let b = i.last.count // b1 printf 1 ; b2 printf 1
+            let an = a * b // an1 printf 1 ; an2 printf 1 
+            answers += an // ansers1 printf 1 ; ansers2 printf 2
+        }
+        return answers // final printf 2
+}
+
+// 后二组选（和前二组选一样）
+let result = binomialCoefficient(n, choose: 2) // prints 10 ,the n max less number 10
+
+// 后二直选（和前二直选一样）
+let 万位 = permutations(n, 1) //n<10
+let 千位 = permutations(n, 1) //n<10
+let 总注数 = 万位 * 千位
+
+// 直选和值 特殊处理
+
+// 后二直选单式(和前二直选单式一样)
+func 后二直选单式(_ n:[Int]) -> Int {
+        var answers:Int = 0
+        for i in n {
+            // i1 = 12  
+            // i2 = 34
+            if i.count != 2 return
+            let a = i.first.count // a1 printf 1 ; a2 printf 1 
+            let b = i.last.count // b1 printf 1 ; b2 printf 1
+            let an = a * b // an1 printf 1 ; an2 printf 1 
+            answers += an // ansers1 printf 1 ; ansers2 printf 2
+        }
+        return answers // final printf 2
+}
+
+//前三组三 
+let 组三 = permutations(n, 2) //n<10
+
+// 前三组六
+let 前三组六 = binomialCoefficient(n, choose: 3) // prints 10 ,n<10
+
+// 前三直选
+let 万位 = permutations(n, 1) //1<n<10
+let 千位 = permutations(n, 1) //1<n<10
+let 百位 = permutations(n, 1) //1<n<10
+let 总注数 = 万位 * 千位 * 百位
+
+// 前三直选单式
+func 前三直选单式(_ n:[Int]) -> Int {
+        var answers:Int = 0
+        for i in n {
+            // i1 = 12  
+            // i2 = 34
+            if i.count != 3 return
+            let a = i.first.count // a1 printf 1 ; a2 printf 1 
+            let b = i[1].count // b1 printf 1 ; b2 printf 1
+            let c = i.last.count
+            let an = a * b * c // an1 printf 1 ; an2 printf 1 
+            answers += an // ansers1 printf 1 ; ansers2 printf 2
+        }
+        return answers // final printf 2
+}
+
+// 中三直选
+let 千位 = permutations(n, 1) //1<n<10
+let 百位 = permutations(n, 1) //1<n<10
+let 十位 = permutations(n, 1) //1<n<10
+let 总注数 = 千位 * 百位 * 十位
+
+// 中三组三（和前三组三一样）
+let  中三组三注数 = permutations(n, 2) //n<10
+
+// 中三直选单式
+func 中三直选单式(_ n:[Int]) -> Int {
+        var answers:Int = 0
+        for i in n {
+            // i1 = 12  
+            // i2 = 34
+            if i.count != 3 return
+            let a = i.first.count // a1 printf 1 ; a2 printf 1 
+            let b = i[1].count // b1 printf 1 ; b2 printf 1
+            let c = i.last.count
+            let an = a * b * c // an1 printf 1 ; an2 printf 1 
+            answers += an // ansers1 printf 1 ; ansers2 printf 2
+        }
+        return answers // final printf 2
+}
+
+// 后三直选
+let 百位 = permutations(n, 1) //1<n<10
+let 十位 = permutations(n, 1) //1<n<10
+let 个位 = permutations(n, 1) //1<n<10
+let 总注数 =  百位 * 十位 * 个位
+
+// 后三组三（和中三组三一样）
+let  中三组三注数 = permutations(n, 2) //n<10
+
+// 后三组六（和前三组六一样）
+let 后三组六 = binomialCoefficient(n, choose: 3) // prints 10 ,n<10
+
+// 后三直选单式(和中三直选单式一样)
+func 后三直选单式(_ n:[Int]) -> Int {
+        var answers:Int = 0
+        for i in n {
+            // i1 = 12  
+            // i2 = 34
+            if i.count != 3 return
+            let a = i.first.count // a1 printf 1 ; a2 printf 1 
+            let b = i[1].count // b1 printf 1 ; b2 printf 1
+            let c = i.last.count
+            let an = a * b * c // an1 printf 1 ; an2 printf 1 
+            answers += an // ansers1 printf 1 ; ansers2 printf 2
+        }
+        return answers // final printf 2
+}
+
+// 四星
+// 后四组选2
+let 后四组选24 = binomialCoefficient(n, choose: 4) // prints 10 ,4<=n<10
+let 后四组选12 =  
+let 后四组选6 = binomialCoefficient(n, choose: 2) // prints 10 ,2<=n<10
+let 后四组选4 = 
+
+// 五星
+let 五星组选120 = binomialCoefficient(n, choose: 5) // prints 10 ,5<=n<10
+let 五星组选60 = 
+let 五星组选30 = 
+let 五星组选20 = 
+let 五星组选10 = 
+let 五星组选5 = 
+
+//五星直选
+let 万位 = permutations(5, 1)
+let 千位 = permutations(2, 1)
+let 百位 = permutations(1, 1)
+let 十位 = permutations(1, 1)
+let 个位 = permutations(1, 1)
+let 注数 = 万位 * 千位 * 百位 * 十位 * 个位
+
+// 五星直选单式
+func 五星直选单式(_ n:[Int]) -> Int {
+        var answers:Int = 0
+        for i in n {
+            if i.count != 5 return
+            let a = i[0].count 
+            let b = i[1].count
+            let c = i[2].count
+            let d = i[3].count
+            let e = i[4].count
+            let an = a * b * c * d *e 
+            answers += an
+        }
+        return answers 
+}
 
 
 /******************testing**************************/
